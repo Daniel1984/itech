70ac5a04266e3322f03e684d125ae3cb8b27c820
Daniel1984 <daniel.stenger@yahoo.com>
2013-03-17 21:30:03 +0000

    test10

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,7 +1,7 @@
 class SubscriberMailer < ActionMailer::Base
-  default from: "info@itech.io"
-
-  def subscribtion_confirmation(email)
-    mail to: email, subject: "iTech Subscription Confirmation"
-  end
+  # default from: "info@itech.io"
+  # 
+  # def subscribtion_confirmation(email)
+  #   mail to: email, subject: "iTech Subscription Confirmation"
+  # end
 end
